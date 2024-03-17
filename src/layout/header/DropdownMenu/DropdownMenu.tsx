2a2a0b08485614d12515a4f08842e14eca03f23a
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-03-17 13:33:36 +0100

    feat: added compoent with test file

---
+++ src/layout/header/DropdownMenu/DropdownMenu.tsx
@@ -0,0 +1,48 @@
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+
+export type MenuItemType = {
+  title: string;
+};
+
+type DropdownMenuProps = {
+  buttonTitle: string;
+  itemTitles: MenuItemType[];
+};
+
+const DropdownMenu = ({ buttonTitle, itemTitles }: DropdownMenuProps) => {
+  return (
+    <Menu>
+      {({ isOpen }) => (
+        <>
+          <MenuButton
+            _active={{ opacity: 0.8 }}
+            _hover={{ opacity: 0.8 }}
+            as={Button}
+            bg="teal"
+            color="white"
+            isActive={isOpen}
+            rightIcon={<ChevronDownIcon />}
+            width="222px"
+          >
+            {buttonTitle}
+          </MenuButton>
+          <MenuList>
+            {itemTitles.map((item) => (
+              <MenuItem
+                key={item.title}
+                _hover={{ opacity: 0.8 }}
+                backgroundColor="teal"
+                color="white"
+              >
+                {item.title}
+              </MenuItem>
+            ))}
+          </MenuList>
+        </>
+      )}
+    </Menu>
+  );
+};
+
+export default DropdownMenu;
