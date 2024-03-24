163dcd28bba41ecd87d33171b8bf9fd8fdc38844
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-03-24 13:40:22 +0100

    feat: configure and add translations

---
+++ src/i18n.ts
@@ -0,0 +1,23 @@
+import { initReactI18next } from "react-i18next";
+import i18n from "i18next";
+import Backend from "i18next-http-backend";
+
+import translations from "./locales/pl/translation.json";
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: "pl",
+    interpolation: {
+      escapeValue: false,
+    },
+    lng: "pl",
+    resources: {
+      pl: {
+        common: translations,
+      },
+    },
+  });
+
+export default i18n;
