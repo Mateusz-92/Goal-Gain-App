82b6f4eb3da61b8c1852928f9fc6b4fcdf4f8801
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-04-22 21:15:34 +0200

    feat: add showcase route

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,16 +3,19 @@ import { createBrowserRouter } from "react-router-dom";
 import { CreateHabits } from "./pages/CreateHabits";
 import { Habits } from "./pages/Habits";
 import { HomePage } from "./pages/HomePage";
+import { Showcase } from "./Showcase";
 
 export const ROUTES = {
   createHabits: "/createHabits",
   habitsEditor: "/habitsEditor",
   habitsTracker: "/habitsTracker",
   home: "/",
+  showcase: "/showcase",
 };
 
 export const router = createBrowserRouter([
   { element: <HomePage />, path: ROUTES.home },
   { element: <CreateHabits />, path: ROUTES.createHabits },
   { element: <Habits />, path: ROUTES.habitsTracker },
+  { element: <Showcase />, path: ROUTES.showcase },
 ]);
