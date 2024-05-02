4544a23ff26d289918fcbe3247f6d6f056166280
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-05-02 21:07:55 +0200

    chore: add new validations

--- src/validators/validators.tsx
+++ src/validators/validators.tsx
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 import i18n from "../i18n";
+
 const minLength: number = 1;
 
 export const taskSchema = z.object({
@@ -29,4 +30,42 @@ export const goalSchema = z.object({
   }),
 });
 
-export type GoalSchema = z.infer<typeof goalSchema>;
+export type GoalFormValuesSchema = z.infer<typeof goalSchema>;
+
+export const MonthlyRatingData = z.object({
+  explanationOfRate: z.string().min(minLength, {
+    message: i18n.t("common:validations.explanationQuestionRequired"),
+  }),
+  lessonOfLife: z.string().min(minLength, {
+    message: i18n.t("common:validations.explanationQuestionRequired"),
+  }),
+  monthsRate: z.string().min(minLength, {
+    message: i18n.t("common:validations.explanationQuestionRequired"),
+  }),
+  theBiggestChalange: z.string().min(minLength, {
+    message: i18n.t("common:validations.explanationQuestionRequired"),
+  }),
+  value: z.string().min(minLength, { message: "Ocena jest wymagana" }),
+});
+
+export type MonthlyValuesRatingSchema = z.infer<typeof MonthlyRatingData>;
+const WeekDaySchema = z.object({
+  date: z.string().min(minLength, { message: "Date is required" }),
+  description: z
+    .string()
+    .min(minLength, { message: "Description is required" }),
+});
+
+export const WeekPlannerDataSchema = z.object({
+  days: z.array(WeekDaySchema),
+  goal1: z.string().min(minLength, { message: "Goal 1 is required" }),
+  goal2: z.string().min(minLength, { message: "Goal 2 is required" }),
+  goal3: z.string().min(minLength, { message: "Goal 3 is required" }),
+  rating: z.string().min(minLength, { message: "Graduate is required" }),
+  week: z.string().min(minLength, { message: "Week is required" }),
+  weekRatingExplanation: z
+    .string()
+    .min(minLength, { message: "Explanation is requaired" }),
+});
+
+export type WeekPlannerData = z.infer<typeof WeekPlannerDataSchema>;
