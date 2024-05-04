502741690693d7facac74b6deea3a611b99ab17f
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-05-04 17:33:37 +0200

    feat: add  file with type

---
+++ src/components/Forms/types.tsx
@@ -0,0 +1,9 @@
+import { Control, FieldValues, Path } from "react-hook-form";
+
+export type BaseInputProps<T extends FieldValues> = {
+  control: Control<T>;
+  label?: string;
+  name: Path<T>;
+};
+
+export type Option = { label: string, value: string; };
