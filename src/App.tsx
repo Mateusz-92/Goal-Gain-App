0136e9104446907dacd7c67b00625b6bea623dbd
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-06-25 17:51:20 +0200

    feat: add providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import { RouterProvider } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
+import UserAvatar from "./components/UserAvatar/UserAvatar";
+import { UserProvider } from "./context/UserContext";
 import { router } from "./routes";
 
 import "./App.css";
-
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 60_000,
+    },
+  },
+  queryCache: new QueryCache(),
+});
 const App = () => {
   return (
     <ChakraProvider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <RouterProvider router={router} />
+          <UserAvatar />
+        </UserProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 };
