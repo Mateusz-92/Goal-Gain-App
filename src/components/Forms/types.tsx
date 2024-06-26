d1ebb1df04ed6f536e560db977148a39d3fa4254
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-06-26 17:25:58 +0200

    chore: changed props to optional property

--- src/components/Forms/types.tsx
+++ src/components/Forms/types.tsx
@@ -1,9 +1,9 @@
 import { Control, FieldValues, Path } from "react-hook-form";
 
 export type BaseInputProps<T extends FieldValues> = {
-  control: Control<T>;
+  control?: Control<T>;
   label?: string;
   name: Path<T>;
 };
 
-export type Option = { label: string, value: string; };
+export type Option = { label: string; value: string };
