60f7268d29c4162b4a735bb946da5fb2bd288701
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-06-30 11:31:17 +0200

    feat: improved funcionality and changed some types

--- src/components/habits/HabitsForm/HabitsForm.tsx
+++ src/components/habits/HabitsForm/HabitsForm.tsx
@@ -1,47 +1,92 @@
 import React from "react";
 import { Checkbox, Table, Tbody, Td, Th, Tr } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
 
-type HabitFormProps = {
+import { DayHabit, HabitFormData } from "../HabitsEditor/HabitsEditor";
+
+const HabitsForm: React.FC<HabitFormData> = ({
+  date,
+  habits,
+}: {
   date: Date;
-  habits: string[];
-};
+  habits: DayHabit;
+}) => {
+  // const numZero = 0;
+  // const startingIndex = 1;
+  function getDayFromDate(dateString: string): string {
+    const date = new Date(dateString);
+    // eslint-disable-next-line no-magic-numbers
+    const day = ("0" + date.getDate()).slice(-2);
+    return day;
+  }
 
-const HabitsForm: React.FC<HabitFormProps> = ({ date, habits }) => {
-  const numZero = 0;
-  const dayCheckboxKeyPrefix = "day-checkbox";
-  const startingIndex = 1;
-  const getDaysInMonth = (date: Date) => {
+  const getDaysInMonth = (dateString: string): string[] => {
+    const date = new Date(dateString);
     const year = date.getFullYear();
     const month = date.getMonth();
-    return new Date(year, month + startingIndex, numZero).getDate();
+    // eslint-disable-next-line no-magic-numbers
+    const lastDay = new Date(year, month + 1, 0).getDate();
+    const days: string[] = [];
+    // eslint-disable-next-line no-magic-numbers
+    for (let i = 2; i <= lastDay + 1; i++) {
+      const day = new Date(year, month, i);
+      // eslint-disable-next-line no-magic-numbers
+      days.push(day.toISOString().split("T")[0]);
+    }
+    return days;
+  };
+  const getDaysInMonthAsString = (date: string): string[] => {
+    const daysInMonth = getDaysInMonth(date);
+    return daysInMonth.map((day) => day.toString());
   };
 
-  const daysInMonth = Array.from(
-    { length: getDaysInMonth(date) },
-    (_, i) => i + startingIndex
-  );
+  const daysInMonthAsString = getDaysInMonthAsString(date.toString());
+
+  function extractHabitsForDate(date: string, allHabits: DayHabit) {
+    // eslint-disable-next-line no-console
+    console.log("allHabits", allHabits);
+    // eslint-disable-next-line no-console
+    console.log("Date", date);
+    // eslint-disable-next-line no-console
+    console.log("allHabits in date", allHabits[date]);
+    return allHabits[date as keyof typeof allHabits]?.habits;
+  }
+
+  function extractHabitNames(data: DayHabit) {
+    const dateKey = Object.keys(data).find((key) => key.includes("-"));
+
+    const habitsArray = data[dateKey as keyof typeof data]?.habits;
+
+    const namesArray = habitsArray.map((habit) => habit.name);
+
+    return namesArray;
+  }
+  const habitNames = extractHabitNames(habits);
 
   return (
     <Table variant="simple" width={"40%"}>
       <Tbody>
         <Tr>
           <Th>Dzień</Th>
-          {habits.map((habit) => (
+          {habitNames.map((habit) => (
             <Th key={habit}>{habit}</Th>
           ))}
         </Tr>
-        {daysInMonth.map((day) => (
-          <Tr key={day}>
-            <Td>{day}</Td>
-            {habits.map((habit, index) => (
-              <Td
-                key={`${day}-${habit}-${dayCheckboxKeyPrefix}-${index + startingIndex}`}
-              >
-                <Checkbox defaultChecked={false} />
-              </Td>
-            ))}
-          </Tr>
-        ))}
+        {daysInMonthAsString.map((day) => {
+          const extractedHabits = extractHabitsForDate(day, habits);
+          return (
+            <Tr key={day}>
+              <Td>{getDayFromDate(day)}</Td>
+              {extractedHabits.map((habit) => {
+                return (
+                  <Td key={habit.id || uuidv4()}>
+                    <Checkbox defaultChecked={habit?.status || false} />
+                  </Td>
+                );
+              })}
+            </Tr>
+          );
+        })}
       </Tbody>
     </Table>
   );
