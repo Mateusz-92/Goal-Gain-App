1896e7c37b22b47877573139345891466191aa4e
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-06-30 11:26:33 +0200

    feat: add paths

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import AuthForm from "./components/AuthForm/AuthForm";
+import ThreeMonthsGoalsPlanner from "./components/Goals/ThreeMonthsGoals/ThreeMonthsGoalsPlanner/ThreeMonthsGoalsPlanner";
+import WeekPlanner from "./components/Goals/WeekPlanner/WeekPlaner";
 import { CreateHabits } from "./pages/CreateHabits";
 import { Habits } from "./pages/Habits";
 import { HomePage } from "./pages/HomePage";
@@ -10,7 +13,11 @@ export const ROUTES = {
   habitsEditor: "/habitsEditor",
   habitsTracker: "/habitsTracker",
   home: "/",
+  login: "/login",
+  register: "/register",
   showcase: "/showcase",
+  threeMonthsGoalsPlanner: "/threeMonthsGoalsPlanner",
+  weekPlanner: "/weekPlanner",
 };
 
 export const router = createBrowserRouter([
@@ -18,4 +25,14 @@ export const router = createBrowserRouter([
   { element: <CreateHabits />, path: ROUTES.createHabits },
   { element: <Habits />, path: ROUTES.habitsTracker },
   { element: <Showcase />, path: ROUTES.showcase },
+  { element: <AuthForm isLogin={false} />, path: ROUTES.register },
+  { element: <AuthForm isLogin={true} />, path: ROUTES.login },
+  {
+    element: <ThreeMonthsGoalsPlanner />,
+    path: ROUTES.threeMonthsGoalsPlanner,
+  },
+  {
+    element: <WeekPlanner />,
+    path: ROUTES.weekPlanner,
+  },
 ]);
