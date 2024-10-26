1b31c7fe959387b453598a668baf9d91be1288ed
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-10-26 13:09:36 +0200

    feat: implement env variables

--- src/firebase/FirebaseConfig.ts
+++ src/firebase/FirebaseConfig.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyDYfXSoxQJ3g51KO_AueRgydnWqrDvBTaE',
-  appId: '1:1002223361472:web:df7d6a090d44ca8dd0fc25',
-  authDomain: 'goal-gain-app.firebaseapp.com',
-  measurementId: 'G-7SSW53KWTB',
-  messagingSenderId: '1002223361472',
-  projectId: 'goal-gain-app',
-  storageBucket: 'goal-gain-app.appspot.com',
+  apiKey: import.meta.env.VITE_API_KEY,
+  appId: import.meta.env.VITE_APP_ID,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
 };
 const firebaseApp = initializeApp(firebaseConfig);
 
