d33121be7bbf66ed86d7dbce19509263e311717c
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-11-05 20:16:21 +0100

    chore: add values, imroved mutation

--- src/components/SavingScratch/ChoiceVariant/ChoiceVariant.tsx
+++ src/components/SavingScratch/ChoiceVariant/ChoiceVariant.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Box, Button, Radio, RadioGroup, useDisclosure } from '@chakra-ui/react';
+import { Box, Radio, RadioGroup, Text, useDisclosure } from '@chakra-ui/react';
 
+import { ammountBord, variant2000, variant3000, variant4000 } from '../../../constants';
 import { useAuth } from '../../../context/AuthContext';
 import { useEditCrossOutSavingComponent } from '../../../firebase/mutations';
+import Btn from '../../../UI/Btn/Btn';
 import ModalApp from '../../Modal/ModalApp';
-import { ammountBord, testBord, testBord2 } from '../CircleList/CircleList';
 
 type BordOption = {
   label: string;
@@ -15,13 +16,14 @@ const ChoiceVariant: React.FC = () => {
   const { isOpen, onClose, onOpen } = useDisclosure();
   const { user } = useAuth();
   const userId = user?.uid || '';
-  const editCrossOutSaving = useEditCrossOutSavingComponent(userId);
+  const { mutate: editCrossOutSaving } = useEditCrossOutSavingComponent();
 
-  const [selectedBord, setSelectedBord] = useState<ammountBord[]>(testBord);
+  const [selectedBord, setSelectedBord] = useState<ammountBord[]>(variant2000);
 
   const bordOptions: BordOption[] = [
-    { label: 'testBord', value: testBord },
-    { label: 'testBord2', value: testBord2 },
+    { label: '2000 PLN oszczędności', value: variant2000 },
+    { label: '3000 PLN oszczędności', value: variant3000 },
+    { label: '4000 PLN oszczędności', value: variant4000 },
   ];
 
   const handleBordChange = (value: string) => {
@@ -37,16 +39,23 @@ const ChoiceVariant: React.FC = () => {
       (option) => JSON.stringify(option.value) === JSON.stringify(selectedBord),
     )?.label;
 
-    editCrossOutSaving.mutate({
-      amounts: selectedBord,
-      variantName: selectedLabel || '',
+    editCrossOutSaving({
+      userId: userId,
+      value: {
+        amounts: selectedBord,
+        variantName: selectedLabel || '',
+      },
     });
     onClose();
   };
-
+  // TODO: text content should be in translations
   return (
     <Box alignItems='center' display='flex' flexDirection='column'>
-      <RadioGroup value={JSON.stringify(selectedBord)} onChange={handleBordChange}>
+      <Text fontWeight={'bold'} m={2} textAlign={'center'}>
+        Wybierz łączną kwotę, ktorą chcesz zaoszczędzić, każdy wariant posiada 33 pola kwot do
+        zaoszczędzenia
+      </Text>
+      <RadioGroup mb={2} value={JSON.stringify(selectedBord)} onChange={handleBordChange}>
         {bordOptions.map((option) => (
           <Radio key={option.label} value={JSON.stringify(option.value)}>
             {option.label}
@@ -54,9 +63,7 @@ const ChoiceVariant: React.FC = () => {
         ))}
       </RadioGroup>
 
-      <Button mt={4} onClick={onOpen}>
-        Save
-      </Button>
+      <Btn text='Zapisz' type='button' onClick={onOpen} />
       <ModalApp
         body={''}
         cancelText='Nie'
