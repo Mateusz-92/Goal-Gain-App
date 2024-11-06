6543b6be30cdd54a0ed12536d8470cc8328ac2ff
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-11-06 18:48:53 +0100

    chore: delete dummy data, assign desired

--- src/components/SavingScratch/CircleList/CircleList.tsx
+++ src/components/SavingScratch/CircleList/CircleList.tsx
@@ -1,81 +1,40 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
-import { v4 as uuidv4 } from 'uuid';
 
 import { useAuth } from '../../../context/AuthContext';
 import { useGetCrossOutAmounts } from '../../../firebase/queries';
 import CircleItem from '../CircleItem/CircleItem';
 
-export type ammountBord = {
-  date?: string;
-  id: string;
-  isCrossOut: boolean;
-  value: number;
-};
-
-export const testBord: ammountBord[] = [
-  {
-    id: uuidv4(),
-    isCrossOut: false,
-    value: 1,
-  },
-  {
-    id: uuidv4(),
-    isCrossOut: false,
-    value: 4,
-  },
-  {
-    id: uuidv4(),
-    isCrossOut: false,
-    value: 6,
-  },
-  {
-    id: uuidv4(),
-    isCrossOut: false,
-    value: 7,
-  },
-];
-export const testBord2: ammountBord[] = [
-  {
-    id: uuidv4(),
-    isCrossOut: false,
-    value: 11,
-  },
-  {
-    id: uuidv4(),
-    isCrossOut: false,
-    value: 41,
-  },
-  {
-    id: uuidv4(),
-    isCrossOut: false,
-    value: 61,
-  },
-  {
-    id: uuidv4(),
-    isCrossOut: false,
-    value: 71,
-  },
-];
-
-const tmpId = 'LAddsyU2qNe5YoxPKfib';
 const CircleList: React.FC = () => {
+  const { crossOutSavingId } = useParams();
   const { user } = useAuth();
   const userId = user?.uid || '';
-  const { data, isError, isLoading } = useGetCrossOutAmounts(tmpId, userId);
+  const { data, isError, isLoading } = useGetCrossOutAmounts(crossOutSavingId || '', userId);
 
   if (isLoading) return <div>isLoading</div>;
   if (isError) return <div>isError</div>;
   if (data)
     return (
-      <Box display='flex' flexWrap='wrap'>
+      <Box
+        display='grid'
+        gap='10px'
+        padding='10px'
+        gridTemplateColumns={{
+          base: 'repeat(auto-fit, minmax(50px, 1fr))',
+          md: 'repeat(7, 1fr)',
+        }}
+        gridTemplateRows={{
+          md: 'repeat(3, auto)',
+        }}
+      >
         {data.map((value) => (
           <CircleItem
             key={value.id}
             amounts={data}
             id={value.id}
             isCrossOut={value.isCrossOut}
-            savingCrossOutId={tmpId}
+            savingCrossOutId={crossOutSavingId}
             value={value.value}
           />
         ))}
