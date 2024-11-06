c15f04710729a54a9e339950d80792b6bf7d5823
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-11-06 18:50:25 +0100

    feat: implement query, add text

--- src/components/SavingScratch/SavingsComponent/SavingsComponent.tsx
+++ src/components/SavingScratch/SavingsComponent/SavingsComponent.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { Box, Text } from '@chakra-ui/react';
 
+import { useAuth } from '../../../context/AuthContext';
+import { useGetCrossOutSavingName } from '../../../firebase/queries';
 import CircleList from '../CircleList/CircleList';
 
 const SavingsComponent: React.FC = () => {
+  const { user } = useAuth();
+  const userId = user?.uid || '';
+  const { crossOutSavingId } = useParams();
+
+  const { data, isError, isLoading } = useGetCrossOutSavingName(userId, crossOutSavingId || '');
+  if (isLoading) return <div>isLoading</div>;
+  if (isError || !data) return <div>isError</div>;
   return (
     <Box>
-      <Text>Skręslaj kwoty co tydzien a w ciągu 3 miesięcy oszczędzisz 2000zł</Text>
+      {data && (
+        <Text fontSize={20} fontWeight={'bold'} textAlign={'center'}>
+          Skreśl 33 kwoty i uzbieraj {data}
+        </Text>
+      )}
       <CircleList />
     </Box>
   );
