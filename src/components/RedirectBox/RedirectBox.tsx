519a9cc2c50ff84df703a5e2de9124909ada3a11
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-11-26 19:35:14 +0100

    feat: add component

---
+++ src/components/RedirectBox/RedirectBox.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { LinkBox, LinkOverlay } from '@chakra-ui/react';
+
+type RedirectBoxProps = {
+  href: string;
+  text: string;
+};
+
+export const RedirectBox: React.FC<RedirectBoxProps> = ({ href, text }) => {
+  return (
+    <LinkBox
+      as='article'
+      borderColor={'var(--dark-gray)'}
+      borderRadius='md'
+      borderWidth='1px'
+      color='var(--dark-gray)'
+      fontWeight='bold'
+      p='4'
+      textAlign={'center'}
+    >
+      <h2>
+        <LinkOverlay href={href}>{text}</LinkOverlay>
+      </h2>
+      <p>Przejdź do kreatora</p>
+    </LinkBox>
+  );
+};
