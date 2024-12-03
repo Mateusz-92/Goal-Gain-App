262c5df378ca7d35996d6bb3fba8379ac1e520ee
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-12-03 19:16:31 +0100

    chore: change stye

--- src/UI/Logo/Logo.tsx
+++ src/UI/Logo/Logo.tsx
@@ -5,11 +5,11 @@ import ggappLogo from '../../assets/ggapp_logo.png';
 export const Logo = () => (
   <Image
     alt='Logo'
-    maxHeight={['20px', '30px', '200px']}
+    maxHeight={['120px', '120px', '200px']}
     maxW='100%'
     objectFit='contain'
     src={ggappLogo}
-    w={['50px', '75px', '200px']} // Adjust width for different screen sizes
+    w={['150px', '150px', '200px']} // Adjust width for different screen sizes
   />
 );
 
