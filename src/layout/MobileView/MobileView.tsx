99e2ef2937cec2afc2099c6efdd356644bfb7c04
Mateusz-92 <matuesz.zasada92@gmail.com>
2024-12-03 19:18:25 +0100

    feat: add mobile view

---
+++ src/layout/MobileView/MobileView.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Box,Flex, VStack } from '@chakra-ui/react';
+
+import { MenuPanel } from '../../components/MenuPanel/MenuPanel';
+import UserAvatar from '../../components/UserAvatar/UserAvatar';
+import Btn from '../../UI/Btn/Btn';
+import Logo from '../../UI/Logo/Logo';
+
+const MobileView: React.FC = () => {
+  const [isToggled, setIsToggled] = useState(true);
+
+  const toggle = () => {
+    setIsToggled((prev) => !prev);
+  };
+
+  return (
+    <Box bg='var(--yellow)' minHeight='100vh' p={25} width='100vw'>
+      <Flex direction='column' justifyContent={'center'}>
+        <Box width='100%'>
+          <Flex justifyContent='center'>
+            <Logo />
+          </Flex>
+          <VStack align='center' mb={15} mt={10}>
+            <Btn type='button' text={isToggled
+? 'Zwiń Panel'
+: 'Otwórz Panel'} onClick={toggle} />
+            {isToggled && (
+              <>
+                <MenuPanel />
+                <Box border='1px solid black' borderRadius={'15px'} p={5}>
+                  <UserAvatar />
+                </Box>
+              </>
+            )}
+          </VStack>
+
+          <Outlet />
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default MobileView;
