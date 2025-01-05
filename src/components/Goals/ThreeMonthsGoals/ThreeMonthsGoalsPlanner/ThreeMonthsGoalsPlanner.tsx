7134905dd4210116aad45c709d085b1d0609cfc3
Mateusz-92 <matuesz.zasada92@gmail.com>
2025-01-05 13:02:29 +0100

    chore: commit

--- src/components/Goals/ThreeMonthsGoals/ThreeMonthsGoalsPlanner/ThreeMonthsGoalsPlanner.tsx
+++ src/components/Goals/ThreeMonthsGoals/ThreeMonthsGoalsPlanner/ThreeMonthsGoalsPlanner.tsx
@@ -43,7 +43,9 @@ const ThreeMonthsGoalsPlanner = ({ mode }: ThreeMonthsGoalsPlannerProps) => {
   const { isOpen, onClose, onOpen } = useDisclosure();
   const editGoalsWithId = useEditGoals(userId, goalId);
   const editGoalsWithoutId = useEditGoals(userId);
-  const onAddGoalsMutation = goalId ? editGoalsWithId : editGoalsWithoutId;
+  const onAddGoalsMutation = goalId
+? editGoalsWithId
+: editGoalsWithoutId;
 
   const {
     control,
@@ -88,13 +90,17 @@ const ThreeMonthsGoalsPlanner = ({ mode }: ThreeMonthsGoalsPlannerProps) => {
     if (goalId) {
       let pointsChange = 0;
       formData.goals.forEach((goal, goalIndex) => {
-        const previousGoal = data ? data[goalIndex] : undefined;
+        const previousGoal = data
+? data[goalIndex]
+: undefined;
 
         if (previousGoal) {
           goal.tasks.forEach((task, taskIndex) => {
             const previousTask = previousGoal.tasks[taskIndex];
             if (previousTask && task.isEnded !== previousTask.isEnded) {
-              pointsChange += task.isEnded ? 25 : -25;
+              pointsChange += task.isEnded
+? 25
+: -25;
             }
           });
         }
@@ -149,7 +155,9 @@ const ThreeMonthsGoalsPlanner = ({ mode }: ThreeMonthsGoalsPlannerProps) => {
               control={control}
               nestedTaskName={`goals.${i}.tasks`}
               register={register}
-              isDisplay={data ? true : false}
+              isDisplay={data
+? true
+: false}
             />
             <TextForm
               control={control}
@@ -189,7 +197,8 @@ const ThreeMonthsGoalsPlanner = ({ mode }: ThreeMonthsGoalsPlannerProps) => {
           />
         </>
       </form>
-      {blocker.state === 'blocked' ? (
+      {blocker.state === 'blocked'
+? (
         <ModalApp
           body={`Masz nie zapisane dane.`}
           cancelText='Nie'
@@ -199,7 +208,8 @@ const ThreeMonthsGoalsPlanner = ({ mode }: ThreeMonthsGoalsPlannerProps) => {
           onClose={() => blocker.reset()}
           onConfirm={() => blocker.proceed()}
         />
-      ) : null}
+      )
+: null}
     </Box>
   );
 };
