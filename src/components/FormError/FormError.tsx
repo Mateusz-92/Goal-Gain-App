a5e16419a4794d96831077a5dcbba688dfdccb3a
Mateusz-92 <matuesz.zasada92@gmail.com>
2025-01-05 13:01:49 +0100

    chore: chane component location

--- src/components/FormError/FormError.tsx
+++ src/components/FormError/FormError.tsx
@@ -2,12 +2,9 @@ import { FieldError } from 'react-hook-form';
 import { FormHelperText } from '@chakra-ui/react';
 
 type FormErrorProps = {
-  error?: FieldError;
+  error: FieldError;
 };
 
 export const FormError = ({ error }: FormErrorProps) => {
-  if (!error) {
-    return null;
-  }
-  return <FormHelperText color='red.500'>{error.message}</FormHelperText>;
+  return  <FormHelperText color='red.500'>{error.message}</FormHelperText>;
 };
